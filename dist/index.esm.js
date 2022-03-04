551bf12ca64be10189d8e50f20f90f0d710174f2
Eran Grinberg <soniceran@gmail.com>
2022-03-04 01:32:46 +0100

    FEATURE | Upgrade packages and merge change from fork origin

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -1,3 +1,29 @@
+function ownKeys(object, enumerableOnly) {
+  var keys = Object.keys(object);
+
+  if (Object.getOwnPropertySymbols) {
+    var symbols = Object.getOwnPropertySymbols(object);
+    enumerableOnly && (symbols = symbols.filter(function (sym) {
+      return Object.getOwnPropertyDescriptor(object, sym).enumerable;
+    })), keys.push.apply(keys, symbols);
+  }
+
+  return keys;
+}
+
+function _objectSpread2(target) {
+  for (var i = 1; i < arguments.length; i++) {
+    var source = null != arguments[i] ? arguments[i] : {};
+    i % 2 ? ownKeys(Object(source), !0).forEach(function (key) {
+      _defineProperty(target, key, source[key]);
+    }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) {
+      Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key));
+    });
+  }
+
+  return target;
+}
+
 function _defineProperty(obj, key, value) {
   if (key in obj) {
     Object.defineProperty(obj, key, {
@@ -13,40 +39,6 @@ function _defineProperty(obj, key, value) {
   return obj;
 }
 
-function ownKeys(object, enumerableOnly) {
-  var keys = Object.keys(object);
-
-  if (Object.getOwnPropertySymbols) {
-    var symbols = Object.getOwnPropertySymbols(object);
-    if (enumerableOnly) symbols = symbols.filter(function (sym) {
-      return Object.getOwnPropertyDescriptor(object, sym).enumerable;
-    });
-    keys.push.apply(keys, symbols);
-  }
-
-  return keys;
-}
-
-function _objectSpread2(target) {
-  for (var i = 1; i < arguments.length; i++) {
-    var source = arguments[i] != null ? arguments[i] : {};
-
-    if (i % 2) {
-      ownKeys(Object(source), true).forEach(function (key) {
-        _defineProperty(target, key, source[key]);
-      });
-    } else if (Object.getOwnPropertyDescriptors) {
-      Object.defineProperties(target, Object.getOwnPropertyDescriptors(source));
-    } else {
-      ownKeys(Object(source)).forEach(function (key) {
-        Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key));
-      });
-    }
-  }
-
-  return target;
-}
-
 var plugin = {
   install: function install(Vue) {
     var _ref = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {},
@@ -56,11 +48,10 @@ var plugin = {
     Vue.mixin({
       data: function data() {
         return {
-          resizeListenerActive: false,
-          debounceTimer: null,
-          eqSize: {
-            width: 0,
-            height: 0
+          $_elementQueryMixin_resizeObserver: null,
+          $_elementQueryMixin_size: {
+            width: null,
+            height: null
           },
           eq: null
         };
@@ -76,13 +67,17 @@ var plugin = {
             // base on the query they have as properties
             var instance = Object.keys(this.eq.breakpoints).reduce(function (accumulator, currentValue) {
               return _objectSpread2(_objectSpread2({}, accumulator), {}, _defineProperty({}, currentValue, _this.$_elementQueryMixin_checkAllConditions(_this.eq.breakpoints[currentValue])));
-            }, {}); // bind public methods to the $eq instance
+            }, {
+              ready: true
+            }); // bind public methods to the $eq instance
 
             instance.forceUpdate = this.$_elementQueryMixin_forceUpdate;
             return instance;
           }
 
-          return {};
+          return {
+            ready: false
+          };
         }
       },
       watch: {
@@ -202,4 +197,4 @@ var plugin = {
   }
 };
 
-export default plugin;
+export { plugin as default };
